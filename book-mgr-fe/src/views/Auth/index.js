892559622d4a934dd8122f8d7def444ce8274c17
yijinss <396049302@qq.com>
2021-02-15 11:08:38 +0800

    完成注册的界面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,16 @@
+import { defineComponent } from 'vue';
+import { UserOutlined } from '@ant-design/icons-vue';
+import { UnlockOutlined } from '@ant-design/icons-vue';
+import { MailOutlined } from '@ant-design/icons-vue';
+<MailOutlined />
+export default defineComponent({
+  components: {
+    UserOutlined,
+    UnlockOutlined,
+    MailOutlined,
+  },
+
+  setup() {
+    
+  }
+})
