0d66e579070e2e93deee0d2cee511590baafbc78
yijinss <396049302@qq.com>
2021-02-17 15:09:55 +0800

    完成了前端图书馆管理基础界面

--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-      Books
+    <a-card>
+      <h2>图书列表</h2>
+      <a-divider></a-divider>
+      
+
+      <space-between>
+        <div class="search">
+          <a-input-search placeholder="根据书名搜索" enter-button/>
+        </div>
+        <a-button>添加一条</a-button>
+      </space-between>
+      <a-divider></a-divider>
+
+      <a-table :columns="columns" :data-source="dataSource ">
+        
+      </a-table>
+    </a-card>
   </div>
 </template>
 
